4348950db5022a0c9d28d5c6b8a852ec6073c9d7
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2020-11-20 16:32:35 +0100

    add host.docker.internal in docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo docker kill ears-server_acquisition ears-server_postgres ears-server_tomcat ears-server_mysql
 sudo docker-compose build &&
 sudo docker-compose up -d
-sleep 70
+sleep 80
 echo Finished! Go to http://localhost:8181/ears3
 echo -ne '\007'
